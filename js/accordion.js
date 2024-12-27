0a1fcba4067b8a125f56e3c06a431c01b454f440
silver-nik <send0@yandex.ru>
2024-12-28 00:25:09 +1000

    fix functions

--- js/accordion.js
+++ js/accordion.js
@@ -2,9 +2,7 @@ const setAccordion = (className) => {
     const accordionItems = document.querySelectorAll(className);
 
     accordionItems.forEach(el => {
-        el.addEventListener("click", (e) => {
-            e.preventDefault();
-
+        el.addEventListener("click", () => {
             accordionItems.forEach(item => {
                 if (item !== el) {
                     item.classList.remove("active");
